ee9abeaebb35d4cd72b050c8bf3cfa97bf340900
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-02-22 17:56:32 +0100

    function to save with switch works

--- test save with switch.py
+++ test save with switch.py
@@ -2,17 +2,10 @@ import json
 import csv
 
 
-with open("leaders.json", "r") as f:
-  leaders_by_country = json.load(f)
 
-print(leaders_by_country)
-  
-#print(leaders_by_country)
-
-# this isn't working, obviously. 
-
-with open("test.csv", "w+", newline="", encoding="utf-8") as f:
-    fieldnames = ["id",
+""" with open("test.csv", "w+", newline="", encoding="utf-8") as f:
+    fieldnames = ["country",
+                  "id",
                   "first_name",
                   "last_name", 
                   "birth_date", 
@@ -23,26 +16,61 @@ with open("test.csv", "w+", newline="", encoding="utf-8") as f:
                   "end_mandate", 
                   "Bio"
                   ]
-    records = json.load(leaders_by_country)
+    
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
-    for row in records:
-        #writer.writerow(row)
-        try:
-            writer.writerow(row)
-        except NameError:
-            print("got a null, keep going")
-            continue
-    
+    for country, leaders_list in leaders_by_country.items():
+        for leader in leaders_list:
+            lead = {k : v for k, v, in leader.items()}
+            lead["country"] = country
+        
+        
+            try:
+                writer.writerow(lead)
+            except NameError:
+                writer.writerow("")
+                continue
+     """
 
 
 
 def save_leader_by_country_with_switch(format=json):
+    with open("leaders.json", "r") as f:
+        leaders_by_country = json.load(f)
+    
     if format == "json":
-            with open("leaders.json", "w") as leaders_json:
+            with open("potato.json", "w") as leaders_json:
                 json.dump(leaders_by_country, leaders_json, indent=2, ensure_ascii=False)
-            with open("leaders.json", "r") as file:
-                print(json.load(file))
-    else: 
-        pass
+
+    
+    if format == "csv":
+        with open("potato.csv", "w+", newline="", encoding="utf-8") as f:
+            fieldnames = ["country",
+                        "id",
+                        "first_name",
+                        "last_name", 
+                        "birth_date", 
+                        "death_date",
+                        "place_of_birth",
+                        "wikipedia_url", 
+                        "start_mandate", 
+                        "end_mandate", 
+                        "Bio"
+                        ]
+    
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for country, leaders_list in leaders_by_country.items():
+                for leader in leaders_list:
+                    lead = {k : v for k, v, in leader.items()}
+                    lead["country"] = country
+
+                    try:
+                        writer.writerow(lead)
+                    except NameError:
+                        writer.writerow("")
+                        continue 
+                
+
+save_leader_by_country_with_switch("csv")
         
